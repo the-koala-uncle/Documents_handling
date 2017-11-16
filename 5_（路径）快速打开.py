45345119bd3ccc0a0ee563686f81daf78f7b09e6
the-koala-uncle <1433289097@qq.com>
2017-11-16 16:02:59 +0800

    Update 5_（路径）快速打开.py

--- 5_（路径）快速打开.py
+++ 5_（路径）快速打开.py
@@ -4,6 +4,8 @@ print('请输入路径代号：')
 
 
 filepath= OrderedDict()
+filepath['bb']=['报表生成','D:\文件储存\~_文件处理区']
+
 filepath['spb']=['审批表','D:/村村通/省级村村通/备查及验收/村村通申请资金/省级村村通项目申请资金']
 filepath['zjbf']=['资金拨付表(财局）','D:\村村通\省级村村通\备查及验收\村村通申请资金']
 filepath['gzsb']=['各镇上报','D:/村村通/省级村村通/上报内容/月报/各镇上报/2017年']
@@ -16,21 +18,26 @@ filepath['tbw']=['通报文','D:\村村通\省级村村通\文件通知（发文
 filepath['tbqm']=['通报签名','D:\局文件、报帐表等\公文处理\廉江市水务局网站信息发布审批表.doc']
 
     
-filepath['syts']=['所有通水','D:\村村通\省级村村通\上报内容\月报\湛江市表']
+filepath['syts']=['所有通水','D:\村村通\省级村村通\上报内容\月报\湛江市表/湛江市村村通自来水工程项目县（市、区）通自来水现状调查统计表(人数）.xlsx']
 filepath['fp']=['精准扶贫','D:\村村通\省级村村通\上报内容\其他\其他报表\精准扶贫']
-    
-filepath['zdx']=['重点县','D:\重点县及水系连通\重点县\重点县2014年/2014年度验收资料']
-filepath['esh']=['20户以上自然村采集表','D:\村村通\省级村村通\上报内容\表格/2017年/20户以上村庄新增村庄各镇确认表(青平、河唇、石颈、城北，安铺没报）']
 
-filepath['直接回车']=['邮箱','']
+filepath['rw']=['任务到村明细表','D:\村村通\省级村村通\备查及验收\村村通备查\任务、资金/任务到村明细（修改2016.11.18）.xls']
+filepath['zdx']=['重点县','D:\重点县及水系连通\重点县\重点县2014年/2014年度验收资料']
+filepath['nyrw']=['廉江市农村饮水任务明细表','D:\农村饮水\新建文件夹\台帐/廉江市农村饮水安全人口一览表.xls']
+filepath['nyhz']=['农村饮水现状普查表','D:\村村通\省级村村通\上报内容\月报\湛江市表\台帐/复件 汇总.xls']
+filepath['esh']=['自然村采集表(20户以上)','D:\村村通\省级村村通\上报内容\表格/2017年/20户以上村庄新增村庄各镇确认表(青平、河唇、石颈、城北，安铺没报）']
+
+
 
 for _ in filepath:
     print(filepath[_][0].ljust(15,'-')+ _)
+print()
+print('村村通总表------（直接回车）')
 path=str(input())
 if path in filepath.keys():
     os.startfile(filepath[path][1].replace('\\','/') )
-if path==''or path=='yx':
-    os.system('"C:/Users\Administrator/AppData/Local/360Chrome/Chrome/Application/360chrome.exe" mail.163.com')
-    sys.exit(1)
-
-
+if path=='':
+    os.startfile('D:/文件储存/2_省级村村通总表.xlsx')
+##if path==''or path=='yx':
+##    os.system('"C:/Users\Administrator/AppData/Local/360Chrome/Chrome/Application/360chrome.exe" mail.163.com')
+##    sys.exit(1)
