059c847be630968d8b9b5738b2a647830ccbaf91
the-koala-uncle <1433289097@qq.com>
2017-12-19 18:19:59 +0800

    Update 1_（xlsx）检查两个文件的不同.py

--- 1_（xlsx）检查两个文件的不同.py
+++ 1_（xlsx）检查两个文件的不同.py
@@ -4,29 +4,32 @@
 import os,sys,openpyxl
 os.chdir(os.getcwd())
 print('请稍等……' )
-list_save=[]
-list3_save=[]
-
 
 ##############################改动的地方######################################
-name1='1.xlsx'
-name2='2.xlsx'
-wb1 = openpyxl.load_workbook(name1)
-wb2 = openpyxl.load_workbook(name2)
-sheet1 = wb1.get_sheet_by_name('Sheet1')
-sheet2 = wb2.get_sheet_by_name('Sheet1')
-#list3_save=['A','B','C','D','E','F','G','H','I','J','K','L','M','N''O','P','Q','R','S','T','U','V','W','X','Y','Z','AA','AB','AC','AD','AE']
-#list3_save=['L','M']
-list3_save=['A','B','C','D','E','F','G','H','I','J','K','L','M','N''O','P','Q','R','S','T','U']
-
-
-
-##############################################################################
-
-print('对比中……' )
 start_row=1
 end_row=29
-print('******'.ljust(10)+name1[:-5].ljust(10)+name2[:-5])
+start_column='A'
+end_column='AE'
+error=0
+#error=0.4
+list3_save=['A','B','C','D','E','F','G','H','I','J','K','L','M','N''O','P','Q','R','S','T','U','V','W','X','Y','Z','AA','AB','AC','AD','AE']
+##############################################################################
+
+list_xlsx=[files for files in os.listdir('.') if files.endswith('.xlsx')]
+if len(list_xlsx)!=2:
+    sys.exit(1)
+name1=list_xlsx[0]
+name2=list_xlsx[1]
+wb1 = openpyxl.load_workbook(name1)
+wb2 = openpyxl.load_workbook(name2)
+sheet1 = wb1.get_sheet_by_name(wb1.get_sheet_names()[0])
+sheet2 = wb2.get_sheet_by_name(wb2.get_sheet_names()[0])
+
+list_save=[]
+list3_save=list3_save[list3_save.index(start_column):list3_save.index(end_column)]
+print('对比中……' )
+print('******'.ljust(10)+name1[:-5].ljust(30)+name2[:-5])
+print('******'.ljust(10)+wb1.get_sheet_names()[0].ljust(30)+wb1.get_sheet_names()[0])
 for index in list3_save:
     for row in range(start_row,end_row+1):
         num =index + str(row)
@@ -38,10 +41,10 @@ for index in list3_save:
                 temp_value1=int(temp_value1)
                 if not (temp_value1==None and temp_value2==0):
                     if not (temp_value2==None and temp_value1==0):
-                        if abs(temp_value1-temp_value2)>1:
+                        if abs(temp_value1-temp_value2)>error:
                             list_save.append(num)
                             try:
-                                print(str(num).ljust(10)+str(temp_value1).ljust(10)+str(temp_value2))
+                                print(str(num).ljust(10)+str(temp_value1).ljust(30)+str(temp_value2))
                             except:
                                 print('')
                                 print(str(num))
@@ -53,7 +56,7 @@ for index in list3_save:
                     if not (temp_value2==None and temp_value1==0):
                             list_save.append(num)
                             try:
-                                print(str(num).ljust(10)+str(temp_value1).ljust(10)+str(temp_value2))
+                                print(str(num).ljust(10)+str(temp_value1).ljust(30)+str(temp_value2))
                             except:
                                 print('')
                                 print(str(num))
